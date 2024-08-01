ff79c743dce06e411d821f02b25e0317f1778d2a
Guilherme Mazzolini <guilhermemazzolini@hotmail.com>
2024-08-01 14:10:02 +0200

    sentry and last comps

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import Navbar from './components/Navbar';
 import Highlights from './components/Highlights';
 import Hero from './components/Hero';
 import Model from './components/Model';
-
+import Features from './components/Features';
+import HowItWorks from './components/HowItWorks';
+import Footer from './components/Footer';
+import * as Sentry from '@sentry/react';
 
 const App = () => {
   return (
@@ -11,8 +14,11 @@ const App = () => {
       <Hero />
       <Highlights />
       <Model />
+      <Features />
+      <HowItWorks />
+      <Footer />
     </main>
   );
 }
 
-export default App;
+export default Sentry.withProfiler(App);
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://c57844a76aa7a38867d327b27cd32bbb@o4507697064509440.ingest.de.sentry.io/4507701468069968",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.browserProfilingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0, 
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  profilesSampleRate: 1.0, 
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0,
+});
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
